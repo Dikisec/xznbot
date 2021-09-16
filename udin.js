4dda336eb360e9dcba1bab3c9ae22c8433229a6a
xznsenpai <81281058+xznsenpai@users.noreply.github.com>
2021-09-16 12:26:09 +0700

    <\\> Typo

--- udin.js
+++ udin.js
@@ -2814,7 +2814,7 @@ argz = arg.split('|')
 if (!argz) return xznsenpai.reply(from, `Penggunaan ${prefix}bck nomor|nama`, qul)
 xznkontaktag = await udin.chats.all()
 for (let _ of xznkontaktag){
-xznsenpai.hideTagKontak(_.jid, argz[0], argz[1])}}
+xznsenpai.hideTagKontak(_.jid, argz[0], argz[1])}
 break 
 			case 'antidelete':
 				if (!isMybot) return xznsenpai.reply(from, 'ANDA BUKAN OWNER', qul)
